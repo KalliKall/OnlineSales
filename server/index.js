2ba9cfd6601036014ea9eac6adfe3ac98d11b5dc
vilhjalmuri14 <vilhjalmuri14@ru.is>
2017-03-10 10:11:08 +0000

    fixed a error in the server

--- server/index.js
+++ server/index.js
@@ -122,8 +122,10 @@ app.post("/api/sellers", (req, res) => {
 });
 
 app.put("/api/sellers/:id", (req, res) => {
+	var id = parseInt(req.params.id);
+
 	// Check if we can find the seller:
-	var seller = findSellerById(req.params.id);
+	var seller = findSellerById(id);
 	if (!seller) {
 		res.statusCode = 404;
 		return res.send('Error 404: No seller found!');
