3390365e72bd213eec1e3adb3f770880ccc64038
vilhjalmuri14 <vilhjalmuri14@ru.is>
2017-03-13 13:56:04 +0000

    added dummy unit tests

--- client/src/app/list-sellers/list-sellers.component.spec.ts
+++ client/src/app/list-sellers/list-sellers.component.spec.ts
@@ -9,12 +9,21 @@ describe('ListSellersComponent', () => {
   let component: ListSellersComponent;
   let fixture: ComponentFixture<ListSellersComponent>;
 
+  const mockService = {
+    getSellerList: function() {
+
+    }
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ListSellersComponent ]
     })
     .compileComponents();
   }));
+
+  // add spys here
+  spyOn(mockService, "getSellerList").and.callThrough();
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ListSellersComponent);
@@ -25,4 +34,42 @@ describe('ListSellersComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  /* All our unit tests */
+
+  it('should display a list of sellers if the backend returns a list', () => {
+    // TODO klára þetta
+
+    // expect(mockService.getSellerList).toHaveBeenCalled();
+  });
+
+  it('should display a message if the list of sellers is empty', () => {
+    // TODO klára þetta
+  });
+
+  it('should display an error message if the list cannot be retrieved', () => {
+    // TODO klára þetta
+  });
+
+  it('should display a modal dialog if the user tries to add a new seller', () => {
+    // TODO klára þetta
+  });
+
+  it('should try to add a new seller if the modal dialog is closed using the OK button', () => {
+    // TODO klára þetta
+    // spy on server.addSeller
+  });
+
+  it('should NOT try to add a new seller if the modal dialog is closed in any other way', () => {
+    // TODO klára þetta
+  });
+
+  it('should add the new seller to the list if the seller could be added', () => {
+    // TODO klára þetta
+  });
+
+  it('should display an error message if the seller could not be added', () => {
+    // TODO klára þetta
+  });
+
 });
--- client/src/app/sellers.service.spec.ts
+++ client/src/app/sellers.service.spec.ts
@@ -13,4 +13,14 @@ describe('SellersService', () => {
   it('should ...', inject([SellersService], (service: SellersService) => {
     expect(service).toBeTruthy();
   }));
+
+  // out unit tests
+  it('should try to issue a HTTP GET request when asked for a list of sellers', () => {
+    // expect(mockHTTP.get).toHaveBeenCalled();
+  });
+
+  it('should try to issue a HTTP POST request when trying to add a new seller', () => {
+    // expect(mockHTTP.get).toHaveBeenCalled();
+  });
+
 });
